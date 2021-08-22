cd3200de34afa9f77e0d1d660b466733c4efc9d8
unknown <calvin.c.r.coderes@gmail.com>
2021-08-22 19:59:14 +0800

    modified context usage from states to reducers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import {GlobalState} from './helpers/Context'
 
 function App() {
+  const {state} = GlobalState()
+  console.log('asd', state)
   return (
     <div className="App">
 
--- src/helpers/Context.js
+++ src/helpers/Context.js
@@ -1,19 +1,50 @@
+/* eslint-disable react/prop-types */
+import {useReducer, useContext} from 'react'
 import {createContext} from "react"
+import {globalReducer} from './Reducers'
 
-export const GlobalState = createContext()
+export const GlobalStates = createContext()
 
 const Context = ({children}) => {
-  const [breedList, setBreedList] = useState([])
-  const [breedDetails, setBreedDetails] = useState({
-    breed: {},
-    image: null,
-    isLoading: true
+  // const [breedList, setBreedList] = useState([])
+  // const [breedDetails, setBreedDetails] = useState({
+  //   breed: {},
+  //   image: null,
+  //   isLoading: true
+  // })
+
+  const [state, dispatch] = useReducer( globalReducer,{
+    breedList: {
+      status: null,
+      isLoading: true,
+      payload: {
+
+      }
+    },
+    breedDetails: {
+      status: null,
+      isLoading: true,
+      payload: {
+        breed: {},
+        image: null
+      }
+    },
+    filter: {
+      filterKeys: '',
+      page: ''
+    }
+
+
   })
   return (
-    <GlobalState.Provider>
+    <GlobalStates.Provider value={{state, dispatch}}>
       {children}
-    </GlobalState.Provider>
+    </GlobalStates.Provider>
   )
 }
 
 export default Context
+
+export const GlobalState = () => {
+  return useContext(GlobalStates)
+}
